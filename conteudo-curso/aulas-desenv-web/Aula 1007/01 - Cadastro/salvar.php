6a59c1bcbef5b7ee364655a210e495679ee6508c
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-07-14 19:55:11 -0300

    .

--- conteudo-curso/aulas-desenv-web/Aula 1007/01 - Cadastro/salvar.php
+++ conteudo-curso/aulas-desenv-web/Aula 1007/01 - Cadastro/salvar.php
@@ -1,4 +1,10 @@
 <?php
-    print_r($_POST);
-    var_dump($_POST);
+    $nome = $_POST['txt-nome'];
+    // $email = $_POST['txt-email'];
+    // $sexo = $_POST['rbt-sexo'];
+    // $nascimento = $_POST['date-nascimento'];
+    // $civil = $_POST['slt-estado-civil'];
+    // $cursos[] = $_POST['check-cursos'];
+
+    echo "Bem-vindo " . $nome;
 ?>
