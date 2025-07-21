dc0f3ca8d43e75585e9e27934629d8a8dce39e0c
zGabrelR1 <gabrielrenostro581@gmail.com>
2025-07-21 20:25:32 -0300

    .

---
+++ conteudo-curso/aulas-desenv-web/Aula 2107/05 -login + menu (Cookies)/login.php
@@ -0,0 +1,30 @@
+<?php
+
+    $usuario = isset($_POST['txt-usuario']) ? $_POST['txt-usuario'] : null; 
+    $senha   = isset($_POST['txt-senha']) ? $_POST['txt-senha'] : null; 
+
+    if ($usuario == null || $senha == null) {
+        echo "<script>
+
+            alert('Preencha todos os campos');
+            window.history.back();
+        </script>";
+        exit;           // Encerra o script
+    }
+
+    // Login   
+    if ($usuario == 'admin' && $senha == '123') {
+        // Abrir sessão
+        session_start();
+        $_SESSION['logado'] = true;
+
+        // Não vamos usar JS para redirecionar para o sistema, vamos usar uma função nativa do PHP
+        header('location: sistema.php');
+    } else {
+        echo "<script>
+
+            alert('Usuário ou senha incorretos!');
+            window.history.back();
+        </script>";
+        exit;
+    }
