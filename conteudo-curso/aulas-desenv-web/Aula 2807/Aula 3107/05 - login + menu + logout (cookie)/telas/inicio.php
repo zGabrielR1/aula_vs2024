359f0b884b9e2974242e25eb04351f166f4496a7
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-07-31 20:37:58 -0300

    .

--- conteudo-curso/aulas-desenv-web/Aula 2807/Aula 3107/05 - login + menu + logout (cookie)/telas/inicio.php
+++ conteudo-curso/aulas-desenv-web/Aula 2807/Aula 3107/05 - login + menu + logout (cookie)/telas/inicio.php
@@ -4,7 +4,9 @@
     ?>
     <h1>Bem-vindo, 
         <!-- desencripta os dados do cookie como ensinado na aula -->
-        <?php echo isset($_SESSION['logado']) ? (isset($_SESSION['usuario']) ? $_SESSION['usuario'] : (isset($_COOKIE['usuario']) ? openssl_decrypt($_COOKIE['usuario'], CIFRA_CRYPT, KEY_CRYPT, 0, VI_CRYPT) : '')) : ''; 
+        <?php 
+        echo isset($_SESSION['logado']) ? (isset($_SESSION['usuario']) ? $_SESSION['usuario'] 
+        : (isset($_COOKIE['usuario']) ? openssl_decrypt($_COOKIE['usuario'], CIFRA_CRYPT, KEY_CRYPT, 0, VI_CRYPT) : '')) : ''; 
     ?>
     </h1>
 </div>
