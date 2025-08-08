d67ba87a566fa1d75fb544b02031bc6110ce035b
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-08-07 21:39:13 -0300

    .

---
+++ conteudo-curso/aulas-desenv-web/Aula 0708/Projeto - Login + BD (blank)/06 - Login + BD/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - EXEMPLO</title>
+    <link href="assets/css/index.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <main class="form-signin w-100 m-auto">
+        <form>
+            <img class="mb-4" src="assets/img/logo.png">
+            <h1 class="h3 mb-3 fw-normal">Faça o login</h1>
+            <div class="form-floating">
+                <input type="text" class="form-control" name="txt-usuario" id="txt-usuario">
+                <label for="txt-usuario">Usuário</label>
+            </div>
+
+            <div class="form-floating input-group mb-3">
+                <input type="password" class="form-control" name="txt-senha" id="txt-senha">
+                <label for="txt-senha">Senha</label>
+                <button class="btn btn-outline-secondary" type="button" id="btn-password" onclick="alternarSenha()"><i id="icon-password" class="bi bi-eye-fill"></i></button>
+            </div>
+            <div class="form-check text-start my-3">
+                <input class="form-check-input" type="checkbox" value="true" name="check_lembrar" id="check_lembrar">
+                <label class="form-check-label" for="check_lembrar">Manter-me conectado</label>
+            </div>
+            <button class="btn btn-primary w-100 py-2" type="submit">Entrar</button>
+            <p class="mt-5 mb-3 text-body-secondary">&copy; 2024</p>
+        </form>
+    </main>
+
+    <!-- JQuery -->
+     <script src="vendor/jquery-3.7.1/jquery-3.7.1.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script>
+        function alternarSenha() {
+            var input = document.getElementById("txt-senha");
+            var icon = document.getElementById("icon-password");
+            
+            if (input.type === "password") {
+                input.type = "text";
+                icon.classList.remove("bi-eye-fill");
+                icon.classList.add("bi-eye-slash-fill");
+            } else {
+                input.type = "password";
+                icon.classList.remove("bi-eye-slash-fill");
+                icon.classList.add("bi-eye-fill");
+            }
+        }
+    </script>
+</body>
+</html>
---
+++ conteudo-curso/aulas-desenv-web/Aula 0708/Projeto - Login + BD (blank)/06 - Login + BD/sistema.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema - EXEMPLO</title>
+    <link href="assets/css/sistema.css" rel="stylesheet">
+</head>
+<body>
+    <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">TEItech</a>
+        <ul class="navbar-nav flex-row d-md-none">
+            <li class="nav-item text-nowrap">
+                <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#sidebarMenu">
+                    <svg class="bi">
+                        <use xlink:href="#list">
+                    </svg>
+                </button>
+            </li>
+        </ul>
+    </header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+                <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="">TEItech</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu"></button>
+                    </div>
+                    <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link d-flex align-items-center gap-2 active" href="#">Clientes</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link d-flex align-items-center gap-2" href="#">Produtos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link d-flex align-items-center gap-2" href="#">Vendas</a>
+                            </li>
+                        </ul>
+                        <hr class="my-3">
+                        <ul class="nav flex-column mb-auto">
+                            <li class="nav-item">
+                                <a class="nav-link d-flex align-items-center gap-2" href="#">Sair</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Bem-vindo!</h1>
+                </div>
+            </main>
+        </div>
+    </div>
+</body>
+</html>
