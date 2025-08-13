89bbff6243653c24065adf2b88ca94420a0effc0
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-08-13 20:11:41 -0300

    .

--- conteudo-curso/aulas-desenv-web/Aula 1108/Projeto - Login + BD (blank)/06 - Login + BD/telas/clientes.php
+++ conteudo-curso/aulas-desenv-web/Aula 1108/Projeto - Login + BD (blank)/06 - Login + BD/telas/clientes.php
@@ -1,6 +1,6 @@
 <form>
     <div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <label for="exampleFormControlInput1" class="form-label">Nome completo</label>
+    <input type="text" class="form-control" id="txt-nome" placeholder="Ex: João Silva">
     </div>
 </form>
