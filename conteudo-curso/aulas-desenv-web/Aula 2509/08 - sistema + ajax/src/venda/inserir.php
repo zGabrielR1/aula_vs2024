754c67aef4810c3aea388c72a18d17f4c0041805
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-10-20 20:16:39 -0300

    ..

--- conteudo-curso/aulas-desenv-web/Aula 2509/08 - sistema + ajax/src/venda/inserir.php
+++ conteudo-curso/aulas-desenv-web/Aula 2509/08 - sistema + ajax/src/venda/inserir.php
@@ -3,57 +3,102 @@
     date_default_timezone_set('America/Sao_Paulo');
 
     // Validação
-    $form['id_cliente'] = $_POST['list-cliente'] ?? null;
-    $form['id_produto'] = $_POST['list-produto'] ?? null;
-    $form['quantidade'] = $_POST['txt-qtd'] ?? null;
-
-    if (in_array(null, $form)) {
+    $id_cliente  = $_POST['id_cliente']   ?? null;
+    $total_venda = $_POST['total_venda']  ?? null;
+    $qtd_total   = $_POST['qtd_total']    ?? null;
+    $produtos    = $_POST['produtos']     ?? [];
+    if (empty($id_cliente) || empty($total_venda) || empty($qtd_total) || count($produtos) == 0) {
         $resposta = [
-            'status'    => 'erro',
-            'mensagem'  => 'Existem dados faltando. Verifique!',
+            'status'   => 'erro',
+            'mensagem' => 'Existem dados faltando. Veifique!',
         ];
         echo json_encode($resposta);
         exit;
     }
     
     // Pegar Data Hora
-    $form['data_hora'] = date('Y-m-d H:i:s');
+    $data_hora = date('Y-m-d H:i:s');
 
-    // Calcular Valor Total da Venda
-    $banco = new BancoDeDados;
-    $produto = $banco->consultar('SELECT preco FROM produtos WHERE id_produto = ?', [$form['id_produto']]);
-    $form['valor_total'] = $form['quantidade'] * $produto['preco'];
-
-    // Gravar a venda no Banco
     try {
-        $sql = 'INSERT INTO vendas (
-            quantidade,
-            valor_total,
-            data_hora,
-            id_produto,
-            id_cliente,
-            cancelado
-        ) VALUES (?,?,?,?,?,?)';
-        $parametros = [
-            $form['quantidade'],
-            $form['valor_total'],
-            $form['data_hora'],
-            $form['id_produto'],
-            $form['id_cliente'],
-            0
-        ];
-        $banco->executarComando($sql, $parametros);
-        $resposta [
-            'status' => 'sucesso',
-            'mensagem' => 'Venda feita com sucesso!'
+        $banco = new BancoDeDados;
 
-        ];
-        window.location.href = '../../sistema.php?tela=vendas';
+        $banco->iniciarTransacao();
 
+        // Validar estoque para cada produto
+        foreach ($produtos as $produto) {
+            $sql = "SELECT estoque FROM produtos WHERE id_produto = ?";
+            $parametros = [$produto['id']];
+            $consulta = $banco->consultar($sql, $parametros);
+            
+            if ($consulta && $consulta['estoque'] < $produto['qtd']) {
+                $resposta = [
+                    'status' => 'erro',
+                    'mensagem' => 'Estoque insuficiente para o produto ID: ' . $produto['id']
+                ];
+                echo json_encode($resposta);
+                exit;
+            } else {
+                // Gravar a venda no Banco
+                $sql = 'INSERT INTO vendas (
+                    qtd_total,
+                    valor_total,
+                    data_hora,
+                    cancelado,
+                    id_cliente
+                ) VALUES (?,?,?,?,?)';
+                $parametros = [
+                    $qtd_total,
+                    $total_venda,
+                    $data_hora,
+                    0,
+                    $id_cliente
+                ];
+                $banco->executarComando($sql, $parametros);
+
+                // Pegar o id_venda recém gravada        
+                $sql = 'SELECT MAX(id_venda) AS ultimo_id_venda FROM vendas';
+                $consulta = $banco->consultar($sql);
+
+                foreach ($produtos as $produto) {
+                    $sql = 'INSERT INTO detalhes (
+                        qtd,
+                        valor,
+                        id_produto,
+                        id_venda
+                    ) VALUES (?,?,?,?)';
+                    $parametros = [
+                        $produto['qtd'],
+                        $produto['valor'],
+                        $produto['id'],
+                        $consulta['ultimo_id_venda']
+                    ];
+                    $banco->executarComando($sql, $parametros);
+                    
+                    // Reduzir o estoque do produto
+                    $sql = 'UPDATE produtos SET estoque = estoque - ? WHERE id_produto = ?';
+                    $parametros = [
+                        $produto['qtd'],
+                        $produto['id']
+                    ];
+                    $banco->executarComando($sql, $parametros);
+                }
+
+                $banco->salvarTRansacao();
+
+                $resposta = [
+                    'status'   => 'sucesso',
+                    'mensagem' => 'Venda cadastrada com sucesso!',
+                ];
+                echo json_encode($resposta);
+                    }
+        }
+        
     } catch(PDOException $erro) {
+        $banco->voltarTransacao();
+
         $resposta = [
-            'status'    => 'erro',
-            'mensagem'  => $erro->getMessage()
+            'status'   => 'erro',
+            'mensagem' => $erro->getMessage(),
         ];
         echo json_encode($resposta);
     }
