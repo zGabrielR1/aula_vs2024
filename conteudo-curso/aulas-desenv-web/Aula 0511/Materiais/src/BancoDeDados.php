de46c620d7fc0cd7ffd7ff9fdd0320826d994692
zGabrielR1 <gabrielrenostro581@gmail.com>
2025-11-05 21:48:58 -0300

    .

--- conteudo-curso/aulas-desenv-web/Aula 0511/Materiais/src/BancoDeDados.php
+++ conteudo-curso/aulas-desenv-web/Aula 0511/Materiais/src/BancoDeDados.php
@@ -2,42 +2,26 @@
     class BancoDeDados {
         // Atributos da Classe
         private $conexao;
-        private $dbName = 'db_tarefas';
-        private $dbUser = 'root';
-        private $dbPassword = 'masterkey';
 
         // Método Construtor (conectar)
         public function __construct() {
-            try {
-                $this->conexao = new PDO('mysql:host=localhost;port=3307;dbname=' . $this->dbName, $this->dbUser, $this->dbPassword);
-                $this->conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                throw new Exception('Erro ao conectar com o banco de dados: ' . $e->getMessage());
-            }
+            $this->conexao = new PDO('mysql:host=localhost;dbname=db_tarefas;charset=utf8mb4', 'root', 'masterkey');
         }
 
         // Método Executar Comandos
         public function executarComando($sql, $parametros = []) {
-            try {
-                $stmt = $this->conexao->prepare($sql);
-                $stmt->execute($parametros);
-            } catch (PDOException $e) {
-                throw new Exception('Erro ao executar comando: ' . $e->getMessage());
-            }
+            $stmt = $this->conexao->prepare($sql);
+            $stmt->execute($parametros);
         }
 
         // Método Consultar
         public function consultar($sql, $parametros = [], $fetchAll = false) {
-            try {
-                $stmt = $this->conexao->prepare($sql);
-                $stmt->execute($parametros);
-                if ($fetchAll) {
-                    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-                } else {
-                    return $stmt->fetch(PDO::FETCH_ASSOC);
-                }
-            } catch (PDOException $e) {
-                throw new Exception('Erro ao consultar: ' . $e->getMessage());
+            $stmt = $this->conexao->prepare($sql);
+            $stmt->execute($parametros);
+            if ($fetchAll) {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                return $stmt->fetch(PDO::FETCH_ASSOC);
             }
         }
         
